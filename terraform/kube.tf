3b26124ad2511b113f9c07152772abe38c0ce30b
Ajay <dev@ajay.app>
2023-03-26 16:03:46 -0400

    more sb server

--- terraform/kube.tf
+++ terraform/kube.tf
@@ -129,7 +129,7 @@ module "kube-hetzner" {
       location    = "ash",
       labels      = [],
       taints      = [],
-      count       = 8
+      count       = 9
     },
     {
       name        = "storage",
